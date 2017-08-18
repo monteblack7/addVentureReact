18647ea108b94420d246b165402188f958e51613
Wyatt Blackhurst <dmonteblackhurst@gmail.com>
2017-08-18 09:50:30 -0600

    switched a to Link tags

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,10 +4,10 @@ class Navbar extends React.Component {
     render() {
         return(
             <ul>
-                <li><a className="active" href="/">Home</a></li>
-                <li><a href="/add">Add Activity</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="#sign-in">Sign In</a></li>
+                <li><Link className="active" to="/">Home</Link></li>
+                <li><Link to="/add">Add Activity</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="#sign-in">Sign In</Link></li>
             </ul>
         )
     }
